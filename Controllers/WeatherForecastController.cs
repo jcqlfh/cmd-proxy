fe3a178d3a113151d45c81d447ef166467d683d4
Jose Candido <candidofilho@gmail.com>
2021-12-14 09:09:52 -0300

    Adding project

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cmd_proxy.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CmdProxyController : ControllerBase
+{
+    private readonly ILogger<CmdProxyController> _logger;
+
+    public CmdProxyController(ILogger<CmdProxyController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Status()
+    {
+        return Ok("ok");
+    }
+
+    [HttpPost]
+    public IActionResult ExecCommand([FromBody] Command command)
+    {
+        string result = "";
+        using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+        {
+            proc.StartInfo.FileName = "/bin/bash";
+            proc.StartInfo.Arguments = "-c \" " + command.Value + " \"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+
+            result += proc.StandardOutput.ReadToEnd();
+            result += proc.StandardError.ReadToEnd();
+
+            proc.WaitForExit();
+        }
+        return Ok(result);
+    }
+}
+
+public struct Command
+{
+    public string Value { get; set; }
+}
